713429ca97afc59da186dd09c0c10bd43568c859
Наиль Шаряпов <zolman99@gmail.com>
2021-03-15 14:37:41 +0300

    Release 2

--- ServiceHardware/Worker.py
+++ ServiceHardware/Worker.py
@@ -27,9 +27,9 @@ def readLight(addr=DEVICE):
   return convertToNumber(data)
 
 
-def get_light():
+def get_light(time):
     value= readLight()
-    x = {"sensorID":"0", "typeSensor":"light", "typeValue":"float", "value": value}
+    x = {"sensorID":"0", "typeSensor":"light", "typeValue":"float", "value": value, "time": str(time)}
     return x
 
 
@@ -59,7 +59,7 @@ def set_pomp():
     return 0
 
 
-sensors_per = [get_light()]#, get_humidity()]
+#sensors_per = [get_light()]#, get_humidity()]
 sensors_req = [get_light_status(), get_pomp_status()]
 controllers = [set_light(), set_pomp()]
 app = Flask(__name__)
@@ -67,8 +67,40 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def hello_world():
-    return json.dumps(sensors_per)
+    import datetime
+    return json.dumps([get_light(datetime.datetime.now())])
 
-
+def signals():
+    from gpiozero import LED
+    red = LED(16)
+    green = LED(20)
+    blue = LED(21)
+    current = red
+    import time
+    while True:
+        val = readLight()
+        if val >20:
+            if current is not blue:
+                current.off()
+                current = blue
+                current.on()
+        elif val > 60:
+            if current is not green:
+                current.off()
+                current = green
+                current.on()
+        else:
+            if current is not red:
+                current.off()
+                current = red
+                current.on()
+        time.sleep(1)
+        
 def exec():
+    import threading
+    thread1 = threading.Thread(target=signals)
+    thread1.start()
     app.run(port=5000)
+
+
+exec()
